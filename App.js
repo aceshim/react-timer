0e7c8d17901552b37edc5597142f5dbc1a396ce4
pensivej <jshim10@illinois.edu>
2018-04-21 14:12:51 -0500

    Import reducer in App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Timer from './components/Timer'
+import reducer from './reducer';
 
 
 export default class App extends React.Component {
